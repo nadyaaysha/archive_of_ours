3d4d31dbcfcb958c28868cfe1bf3e7eee71c7791
nadyaaysha <nadyaaysha07@gmail.com>
2023-09-23 20:23:13 +0700

    T4

--- main/forms.py
+++ main/forms.py
@@ -4,5 +4,5 @@ from main.models import Item
 class ProductForm(ModelForm):
     class Meta:
         model = Item
-        fields = ["name", "amount", "description", "word_Count", "genre", "character_Source"]
+        fields = ["name", "title", "amount", "description", "word_Count", "genre", "character_Source"]
         
